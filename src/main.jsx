962503412a9c3902a2630a13a098c157ad528e1e
SamuelInocencio <samuelferreirainocencio@gmail.com>
2025-02-03 18:22:32 -0300

    concluida aula GlobalStyles

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Home from './Home'
+import { GlobalStyles } from './styles/GlobalStyes'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <GlobalStyles />
     <Home />
   </StrictMode>,
 )
